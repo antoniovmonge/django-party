4d4c6ea9dbd9199ac20ee108814c3c1993fc7154
Antonio Vilchez <antoniovmonge@gmail.com>
2023-12-15 13:00:18 +0100

    add search and filter

--- core/party/views/__init__.py
+++ core/party/views/__init__.py
@@ -5,7 +5,7 @@ from .gift_registry_views import (
     GiftUpdateFormPartial,
     delete_gift_partial,
 )
-from .guest_list_views import GuestListPage, mark_attending_partial, mark_not_attending_partial
+from .guest_list_views import GuestListPage, filter_guests_partial, mark_attending_partial, mark_not_attending_partial
 from .new_party_views import page_new_party, partial_check_invitation, partial_check_party_date
 from .party_detail_views import PartyDetailPage, PartyDetailPartial
 from .party_list_views import PartyListPage
@@ -25,4 +25,5 @@ __all__ = [
     "GuestListPage",
     "mark_attending_partial",
     "mark_not_attending_partial",
+    "filter_guests_partial",
 ]
