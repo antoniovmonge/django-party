af7baf6f80633b643e72ec2fa3f8c7c5e2a5c6c1
Antonio Vilchez <antoniovmonge@gmail.com>
2023-12-15 10:56:15 +0100

    add new functionality ADD gift with HTMX

--- core/party/urls.py
+++ core/party/urls.py
@@ -24,6 +24,7 @@ gift_registry_urlpatterns = [
     path("gifts/<uuid:gift_uuid>/", views.GiftDetailPartial.as_view(), name="partial_gift_detail"),
     path("gifts/<uuid:gift_uuid>/form/", views.GiftUpdateFormPartial.as_view(), name="partial_gift_update"),
     path("gifts/<uuid:gift_uuid>/delete/", views.delete_gift_partial, name="partial_gift_delete"),
+    path("<uuid:party_uuid>/new-gift/", views.GiftCreateFormPartial.as_view(), name="partial_new_gift"),
 ]
 
 urlpatterns = list_parties_urlpatterns + party_detail_urlpatterns + new_party_urlpatterns + gift_registry_urlpatterns
--- core/party/views/__init__.py
+++ core/party/views/__init__.py
@@ -1,4 +1,10 @@
-from .gift_registry_views import GiftDetailPartial, GiftRegistryPage, GiftUpdateFormPartial, delete_gift_partial
+from .gift_registry_views import (
+    GiftCreateFormPartial,
+    GiftDetailPartial,
+    GiftRegistryPage,
+    GiftUpdateFormPartial,
+    delete_gift_partial,
+)
 from .new_party_views import page_new_party, partial_check_invitation, partial_check_party_date
 from .party_detail_views import PartyDetailPage, PartyDetailPartial
 from .party_list_views import PartyListPage
@@ -14,4 +20,5 @@ __all__ = [
     "GiftUpdateFormPartial",
     "GiftDetailPartial",
     "delete_gift_partial",
+    "GiftCreateFormPartial",
 ]
