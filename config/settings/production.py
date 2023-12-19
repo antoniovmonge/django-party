f38ee90bd00df95fc29e999bdb28b20c3c7407eb
Antonio Vilchez <antoniovmonge@gmail.com>
2023-12-19 19:23:39 +0100

    remove-docs and set socialaccount-auth via settings

--- config/settings/production.py
+++ config/settings/production.py
@@ -6,7 +6,7 @@ from .base import env
 # https://docs.djangoproject.com/en/dev/ref/settings/#secret-key
 SECRET_KEY = env("DJANGO_SECRET_KEY")
 # https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = env.list("DJANGO_ALLOWED_HOSTS", default=["core.com"])
+ALLOWED_HOSTS = env.list("DJANGO_ALLOWED_HOSTS", default=["django-party.com"])
 
 # DATABASES
 # ------------------------------------------------------------------------------
@@ -95,14 +95,14 @@ MEDIA_URL = f"https://{aws_s3_domain}/media/"
 # https://docs.djangoproject.com/en/dev/ref/settings/#default-from-email
 DEFAULT_FROM_EMAIL = env(
     "DJANGO_DEFAULT_FROM_EMAIL",
-    default="core <noreply@core.com>",
+    default="Django-Party <noreply@django-party.com>",
 )
 # https://docs.djangoproject.com/en/dev/ref/settings/#server-email
 SERVER_EMAIL = env("DJANGO_SERVER_EMAIL", default=DEFAULT_FROM_EMAIL)
 # https://docs.djangoproject.com/en/dev/ref/settings/#email-subject-prefix
 EMAIL_SUBJECT_PREFIX = env(
     "DJANGO_EMAIL_SUBJECT_PREFIX",
-    default="[core] ",
+    default="[Django-Party] ",
 )
 
 # ADMIN
@@ -173,7 +173,7 @@ LOGGING = {
 # -------------------------------------------------------------------------------
 # Tools that generate code samples can use SERVERS to point to the correct domain
 SPECTACULAR_SETTINGS["SERVERS"] = [  # noqa: F405
-    {"url": "https://core.com", "description": "Production server"},
+    {"url": "https://django-party.com", "description": "Production server"},
 ]
 # Your stuff...
 # ------------------------------------------------------------------------------
